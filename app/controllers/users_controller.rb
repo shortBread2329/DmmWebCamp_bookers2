9a14d3e1211aef2ac0cc94825b451a40e3b233bf
EC2 Default User <ec2-user@ip-172-31-48-215.ec2.internal>
2021-05-06 14:15:48 +0000

    relationshipモデルを作成 #5

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,40 +1,49 @@
 class UsersController < ApplicationController
-    def index
-        @users = User.page(params[:page]).reverse_order
-        @user = current_user
-        @book = Book.new
+  def index
+    @users = User.page(params[:page]).reverse_order
+    @user = current_user
+    @book = Book.new
+  end
+  
+  def show
+    @user = User.find(params[:id])
+    @book = Book.new
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+    if @user != current_user
+      flash[:alert] = "You can't edit other users' infomation."
+      @users = User.page(params[:page]).reverse_order
+      @user = current_user
+      @book = Book.new
+      redirect_to user_path(current_user)
     end
-    
-    def show
-        @user = User.find(params[:id])
-        @book = Book.new
+  end
+  
+  def update
+    user = User.find(params[:id])
+    if user.update(user_params)
+      flash[:notice] = "You have updated user successfully."
+      redirect_to user_path(user)
+    else
+      @user = user
+      render :edit
     end
-    
-    def edit
-        @user = User.find(params[:id])
-        if @user != current_user
-            flash[:alert] = "You can't edit other users' infomation."
-            @users = User.page(params[:page]).reverse_order
-            @user = current_user
-            @book = Book.new
-            redirect_to user_path(current_user)
-        end
-    end
-    
-    def update
-       user = User.find(params[:id])
-        if user.update(user_params)
-            flash[:notice] = "You have updated user successfully."
-            redirect_to user_path(user)
-        else
-            @user = user
-            render :edit
-        end
-    end
-    
-    private
-    
-    def user_params
-        params.require(:user).permit(:name, :profile_image, :introduction)
-    end
+  end
+  def follows
+    @user = User.find(params[:id])
+    @users = @user.follows
+  end
+  
+  def followers
+    @user = User.find(params[:id])
+    @users = @user.followers
+  end
+
+  private
+  
+  def user_params
+      params.require(:user).permit(:name, :profile_image, :introduction)
+  end
 end
