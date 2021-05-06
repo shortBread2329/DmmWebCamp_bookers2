1d7547faf29fd309757c9bc221e67ed65fe5a29b
EC2 Default User <ec2-user@ip-172-31-48-215.ec2.internal>
2021-05-06 14:23:22 +0000

    Relationshipモデル修正 #5

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,2 +1,10 @@
 class Relationship < ApplicationRecord
+  belongs_to :follower, class_name: "User"
+  belongs_to :followed, class_name: "User"
+  validates :follower_id, presence: true
+  validates :followed_id, presence: true
+
+  def follow(other_user)
+    active_relationships.create(followed_id: other_user.id)
+  end
 end
